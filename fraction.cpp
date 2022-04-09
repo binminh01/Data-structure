d47d7078457a2e5dd957394b35c59302982081ac
binminh01 <88193692+binminh01@users.noreply.github.com>
2022-04-09 09:33:29 +0700

    Update fraction.cpp

--- fraction.cpp
+++ fraction.cpp
@@ -5,12 +5,21 @@ struct fraction {
     fraction(int x, int y) {assert(y != 0); a = x; b = y; int c = __gcd(a, b); a/=c; b/=c;}
     friend istream& operator>>(istream& in, fraction& f) {in >> f.a >> f.b; return in;}
     friend ostream& operator<<(ostream& out, fraction& f) {out << f.a << ' ' << f.b; return out;}
-    bool operator<(fraction& f) {return a*f.b < b*f.a;}
-    bool operator<=(fraction& f) {return a*f.b <= b*f.a;}
-    bool operator>(fraction& f) {return a*f.b > b*f.a;}
-    bool operator>=(fraction& f) {return a*f.b >= b*f.a;}
-    fraction operator+(fraction& f) {return fraction(a*f.b + b*f.a, b*f.b);}
-    fraction operator-(fraction& f) {return fraction(a*f.b - b*f.a, b*f.b);}
-    fraction operator*(fraction& f) {return fraction(a*f.a, b*f.b);}
-    fraction operator/(fraction& f) {return fraction(a*f.b, b*f.a);}
+    bool operator==(fraction f) {return a == f.a && b == f.b;}
+    bool operator<(fraction f) {return a*f.b < b*f.a;}
+    bool operator<=(fraction f) {return a*f.b <= b*f.a;}
+    bool operator>(fraction f) {return a*f.b > b*f.a;}
+    bool operator>=(fraction f) {return a*f.b >= b*f.a;}
+    fraction operator+(fraction f) {return fraction(a*f.b + b*f.a, b*f.b);}
+    fraction operator-(fraction f) {return fraction(a*f.b - b*f.a, b*f.b);}
+    fraction operator*(fraction f) {return fraction(a*f.a, b*f.b);}
+    fraction operator/(fraction f) {return fraction(a*f.b, b*f.a);}
+    fraction operator+(int n) {return fraction(a + b*n, b);}
+    fraction operator-(int n) {return fraction(a - b*n, b);}
+    fraction operator*(int n) {return fraction(a*n, b);}
+    fraction operator/(int n) {return fraction(a, b*n);}
+    void operator+=(fraction f) {a = a*f.b + b*f.a, b*=f.b; int c = __gcd(a, b); a/=c; b/=c;}
+    void operator-=(fraction f) {a = a*f.b - b*f.a, b*=f.b; int c = __gcd(a, b); a/=c; b/=c;}
+    void operator*=(fraction f) {a*=f.a, b*=f.b; int c = __gcd(a, b); a/=c; b/=c;}
+    void operator/=(fraction f) {a*=f.b; b*=f.a; int c = __gcd(a, b); a/=c; b/=c;}
 };
