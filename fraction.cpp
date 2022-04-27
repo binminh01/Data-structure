265c0c35b42f0dcea0459cd9a66f54e35d0aee53
binminh01 <88193692+binminh01@users.noreply.github.com>
2022-04-27 23:06:48 +0700

    Update fraction.cpp

--- fraction.cpp
+++ fraction.cpp
@@ -10,18 +10,18 @@ struct fraction {
     }
     friend istream& operator>>(istream& in, fraction f) {in >> f.a >> f.b; return in;}
     friend ostream& operator<<(ostream& out, fraction f) {out << f.a << '/' << f.b; return out;}
-    bool operator==(fraction f) {return a == f.a && b == f.b;}
-    bool operator<(fraction f) {return a*f.b < b*f.a;}
-    bool operator<=(fraction f) {return a*f.b <= b*f.a;}
-    bool operator>(fraction f) {return a*f.b > b*f.a;}
-    bool operator>=(fraction f) {return a*f.b >= b*f.a;}
-    fraction operator+(fraction f) {return fraction(a*f.b + b*f.a, b*f.b);}
-    fraction operator-(fraction f) {return fraction(a*f.b - b*f.a, b*f.b);}
-    fraction operator*(fraction f) {return fraction(a*f.a, b*f.b);}
-    fraction operator/(fraction f) {return fraction(a*f.b, b*f.a);}
-    fraction operator+(int n) {return fraction(a + b*n, b);}
-    fraction operator-(int n) {return fraction(a - b*n, b);}
-    fraction operator*(int n) {return fraction(a*n, b);}
-    fraction operator/(int n) {return fraction(a, b*n);}
+    bool operator==(fraction f) const {return a == f.a && b == f.b;}
+    bool operator<(fraction f) const {return a*f.b < b*f.a;}
+    bool operator<=(fraction f) const {return a*f.b <= b*f.a;}
+    bool operator>(fraction f) const {return a*f.b > b*f.a;}
+    bool operator>=(fraction f) const {return a*f.b >= b*f.a;}
+    fraction operator+(fraction f) const {return fraction(a*f.b + b*f.a, b*f.b);}
+    fraction operator-(fraction f) const {return fraction(a*f.b - b*f.a, b*f.b);}
+    fraction operator*(fraction f) const {return fraction(a*f.a, b*f.b);}
+    fraction operator/(fraction f) const {return fraction(a*f.b, b*f.a);}
+    fraction operator+(int n) const {return fraction(a + b*n, b);}
+    fraction operator-(int n) const {return fraction(a - b*n, b);}
+    fraction operator*(int n) const {return fraction(a*n, b);}
+    fraction operator/(int n) const {return fraction(a, b*n);}
 };
 using F = fraction;
