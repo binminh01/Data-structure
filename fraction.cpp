421f8e20f9d45760e8bd733993017ec13808a8cb
binminh01 <88193692+binminh01@users.noreply.github.com>
2022-04-27 23:06:05 +0700

    Update fraction.cpp

--- fraction.cpp
+++ fraction.cpp
@@ -1,32 +1,27 @@
 struct fraction {
     int a, b;
-    fraction(int x, int y) {
+    fraction(int x, int y = 1) {
         assert(y != 0);
         a = x; b = y;
-        int c = __gcd(a, b);
+        int c = __gcd(abs(a), abs(b));
         a/=c;
         b/=c;
         if (b < 0) a = -a, b = -b;
     }
     friend istream& operator>>(istream& in, fraction f) {in >> f.a >> f.b; return in;}
-    friend ostream& operator<<(ostream& out, fraction f) {out << f.a << ' ' << f.b; return out;}
-    bool operator==(fraction &f) {return a == f.a && b == f.b;}
-    bool operator<(const fraction &f) {return a*f.b < b*f.a;}
-    bool operator<=(const fraction &f) {return a*f.b <= b*f.a;}
-    bool operator>(const fraction &f) {return a*f.b > b*f.a;}
-    bool operator>=(const fraction &f) {return a*f.b >= b*f.a;}
-    fraction operator+(const fraction &f) {return fraction(a*f.b + b*f.a, b*f.b);}
-    fraction operator-(const fraction &f) {return fraction(a*f.b - b*f.a, b*f.b);}
-    fraction operator*(const fraction &f) {return fraction(a*f.a, b*f.b);}
-    fraction operator/(const fraction &f) {return fraction(a*f.b, b*f.a);}
+    friend ostream& operator<<(ostream& out, fraction f) {out << f.a << '/' << f.b; return out;}
+    bool operator==(fraction f) {return a == f.a && b == f.b;}
+    bool operator<(fraction f) {return a*f.b < b*f.a;}
+    bool operator<=(fraction f) {return a*f.b <= b*f.a;}
+    bool operator>(fraction f) {return a*f.b > b*f.a;}
+    bool operator>=(fraction f) {return a*f.b >= b*f.a;}
+    fraction operator+(fraction f) {return fraction(a*f.b + b*f.a, b*f.b);}
+    fraction operator-(fraction f) {return fraction(a*f.b - b*f.a, b*f.b);}
+    fraction operator*(fraction f) {return fraction(a*f.a, b*f.b);}
+    fraction operator/(fraction f) {return fraction(a*f.b, b*f.a);}
     fraction operator+(int n) {return fraction(a + b*n, b);}
     fraction operator-(int n) {return fraction(a - b*n, b);}
     fraction operator*(int n) {return fraction(a*n, b);}
     fraction operator/(int n) {return fraction(a, b*n);}
-    void operator+=(fraction &f) {a = a*f.b + b*f.a, b*=f.b; int c = __gcd(a, b); a/=c; b/=c;}
-    void operator-=(fraction &f) {a = a*f.b - b*f.a, b*=f.b; int c = __gcd(a, b); a/=c; b/=c;}
-    void operator*=(fraction &f) {a*=f.a, b*=f.b; int c = __gcd(a, b); a/=c; b/=c;}
-    void operator/=(fraction &f) {a*=f.b; b*=f.a; int c = __gcd(a, b); a/=c; b/=c;}
-    friend fraction max(fraction &a, fraction &b) {return a > b ? a: b;}
-    friend fraction min(fraction &a, fraction &b) {return a < b ? a: b;}
 };
+using F = fraction;
