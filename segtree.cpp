b2acadfd72250f430d7b09233cb754b603de0ef3
binminh01 <88193692+binminh01@users.noreply.github.com>
2022-05-15 18:44:05 +0700

    Update segtree.cpp

--- segtree.cpp
+++ segtree.cpp
@@ -1,40 +1,38 @@
 struct segtree {
-    int sz;
+    int n;
     vector<int> t;
-    void init(int n) {
-        sz = 1;
-        while (sz < n) sz <<= 1;
-        t.assign(sz << 1, 0ll);
-    }
-    void build(int n, vector<int> &a, int x, int lx, int rx) {
-        if (rx - lx == 1) {
-            if (lx < n) t[x] = a[lx];
+    int neutral = 0;
+    int merge(int a, int b) {return a + b;}
+    void init(int N) {n = N; t.assign(4*n, 0ll);}
+    void build(vector<int> &a, int x, int lx, int rx) {
+        if (lx == rx) {
+            t[x] = a[lx];
             return;
         }
-        int m = (lx + rx) >> 1;
-        build(n, a, (x << 1) + 1, lx, m);
-        build(n, a, (x << 1) + 2, m, rx);
-        t[x] = t[(x << 1) + 1] + t[(x << 1) + 2];
+        int m = (lx + rx)/2;
+        build(a, 2*x + 1, lx, m);
+        build(a, 2*x + 2, m + 1, rx);
+        t[x] = merge(t[2*x + 1], t[2*x + 2]);
     }
     void set(int i, int v, int x, int lx, int rx) {
-        if (rx - lx == 1) {
+        if (lx == rx) {
             t[x] = v;
             return;
         }
-        int m = (lx + rx) >> 1;
-        if (i < m) set(i, v, (x << 1) + 1, lx, m);
-        else set(i, v, (x << 1) + 2, m, rx);
-        t[x] = t[(x << 1) + 1] + t[(x << 1) + 2];
+        int m = (lx + rx)/2;
+        if (i <= m) set(i, v, 2*x, lx, m);
+        else set(i, v, 2*x + 1, m + 1, rx);
+        t[x] = merge(t[2*x], t[2*x + 1]);
     }
     int get(int l, int r, int x, int lx, int rx) {
-        if (lx >= r || l >= rx) return 0;
+        if (lx > r || rx < l) return neutral;
         if (lx >= l && rx <= r) return t[x];
-        int m = (lx + rx) >> 1;
-        return get(l, r, (x << 1) + 1, lx, m) + get(l, r, (x << 1) + 2, m, rx);
+        int m = (lx + rx)/2;
+        return merge(get(l, r, 2*x, lx, m), get(l, r, 2*x + 1, m + 1, rx));
     }
-    void build(int n, vector<int> &a) {build(n, a, 0, 0, sz);}
-    void set(int i, int v) {set(i, v, 0, 0, sz);}
-    int get(int l, int r) {return get(l, r, 0, 0, sz);}
+    void build(vector<int> &a) {build(a, 1, 1, n);}
+    void set(int i, int v) {set(i, v, 1, 1, n);}
+    int get(int l, int r) {return get(l, r, 1, 1, n);}
 };
 struct segtree_lazy {
     int n;
