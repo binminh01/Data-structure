eb8d48e9dbbbfefe37fbedbf29a5dad20f236c61
binminh01 <88193692+binminh01@users.noreply.github.com>
2022-05-15 18:12:31 +0700

    Update segtree.cpp

--- segtree.cpp
+++ segtree.cpp
@@ -36,3 +36,43 @@ struct segtree {
     void set(int i, int v) {set(i, v, 0, 0, sz);}
     int get(int l, int r) {return get(l, r, 0, 0, sz);}
 };
+struct segtree_lazy {
+    int n;
+    vector<int> t, la;
+    void init(int N) {
+        n = N;
+        t.assign(4*n, 0ll);
+        la.assign(4*n, 0ll);
+    }
+    void build(vector<int> &a, int x, int lx, int rx) {
+        if (lx == rx) {
+            t[x] = a[lx];
+            return;
+        }
+        int m = (lx + rx)/2;
+        build(a, 2*x, lx, m);
+        build(a, 2*x + 1, m + 1, rx);
+        t[x] = max(t[2*x], t[2*x + 1]);
+    }
+    void down(int i) {
+        t[2*i]+=la[i], t[2*i + 1]+=la[i];
+        la[2*i]+=la[i], la[2*i + 1]+=la[i];
+        la[i] = 0;
+    }
+    void set(int l, int r, int x, int lx, int rx, int d) {
+        if (lx > r || rx < l) return;
+        if (lx >= l && rx <= r) {t[x]+=d; la[x]+=d; return;}
+        down(x);
+        int m = (lx + rx)/2;
+        set(l, r, 2*x, lx, m, d);
+        set(l, r, 2*x + 1, m + 1, rx, d);
+        t[x] = max(t[2*x], t[2*x + 1]);
+    }
+    int get(int l, int r, int x, int lx, int rx) {
+        if (lx > r || rx < l) return 0;
+        if (lx >= l && rx <= r) return t[x];
+        down(x);
+        int m = (lx + rx)/2;
+        return max(get(l, r, 2*x, lx, m), get(l, r, 2*x + 1, m + 1, rx));
+    }
+};
