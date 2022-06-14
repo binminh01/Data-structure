64e25bcf6fea46ef043d8cde4688b4332a64f2c4
binminh01 <88193692+binminh01@users.noreply.github.com>
2022-06-14 19:58:52 +0700

    Update BIT.cpp

--- BIT.cpp
+++ BIT.cpp
@@ -1,25 +1,22 @@
 struct BIT {
     int n;
     vector<int> bit;
-    void init(int n) {
-        this->n = n;
+    void init(int N) {
+        n = N;
         bit.assign(n + 1, 0);
     }
-    void build(int n, vector<int> &a) {
-        this->n = n;
-        bit.assign(n + 1, 0);
-        for (int i = 0; i < n; i++) set(i, a[i]);
+    void build(vector<int> &a) {
+        for (int i = 1; i <= n; i++) set(i, a[i]);
     }
     void set(int i, int v) {
-        for (i++; i <= n; i+=i & (-i)) bit[i]+=v;
+        for (; i <= n; i+=i & (-i)) bit[i]+=v;
     }
     int get(int i) {
         int x = 0;
-        for (i++; i; i-=i & (-i)) x+=bit[i];
+        for (; i; i-=i & (-i)) x+=bit[i];
         return x;
     }
     int get(int l, int r) {
-        if (l == 0) return get(r);
         return get(r) - get(l - 1);
     }
-};
+} bit;
