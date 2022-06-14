51044de4933af64f22aa1f3f43618dad46e27a1e
binminh01 <88193692+binminh01@users.noreply.github.com>
2022-06-14 20:34:19 +0700

    Update BIT.cpp

--- BIT.cpp
+++ BIT.cpp
@@ -1,22 +1,23 @@
+template<class T>
 struct BIT {
     int n;
-    vector<int> bit;
+    vector<T> bit;
     void init(int N) {
         n = N;
-        bit.assign(n + 1, 0);
+        bit.resize(n + 1);
     }
-    void build(vector<int> &a) {
+    void build(const vector<T> &a) {
         for (int i = 1; i <= n; i++) set(i, a[i]);
     }
-    void set(int i, int v) {
+    void set(int i, T v) {
         for (; i <= n; i+=i & (-i)) bit[i]+=v;
     }
-    int get(int i) {
-        int x = 0;
+    T get(int i) {
+        T x = 0;
         for (; i; i-=i & (-i)) x+=bit[i];
         return x;
     }
-    int get(int l, int r) {
+    T get(int l, int r) {
         return get(r) - get(l - 1);
     }
-} bit;
+};
