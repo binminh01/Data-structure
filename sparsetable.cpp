9cbb6f4bcc247e2389fe2572c743dc8363c4ee2e
binminh01 <88193692+binminh01@users.noreply.github.com>
2023-01-19 09:19:59 +0700

    Update sparsetable.cpp

--- sparsetable.cpp
+++ sparsetable.cpp
@@ -1,18 +1,16 @@
 int Log2(int n) {
-    int x = 0;
-    while ((1 << x) <= n) x++;
-    return x - 1;
+    return 31 - __builtin_clz(n);
 }
-int st[200001][20];
+int st[20][200001];
 void build(vi &a, int n) {
-    for (int i = 0; i < n; i++) st[i][0] = a[i];
+    for (int i = 0; i < n; i++) st[0][i] = a[i];
     for (int j = 1; (1 << j) <= n; j++) {
         for (int i = 0; i <= n - (1 << j); i++) {
-            st[i][j] = min(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);
+            st[j][i] = min(st[j - 1][i], st[j - 1][i + (1 << (j - 1))]);
         }
     }
 }
 int query(int l, int r) {
-    int i = (int)Log2(r - l + 1);
-    return min(st[l][i], st[r - (1 << i) + 1][i]);
+    int i = Log2(r - l + 1);
+    return min(st[i][l], st[i][r - (1 << i) + 1]);
 }
