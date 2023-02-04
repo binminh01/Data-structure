3fe2db04ad9782ca60eb21d0ceb94607e1798c97
binminh01 <88193692+binminh01@users.noreply.github.com>
2023-02-04 17:31:08 +0700

    Create dsu.cpp

---
+++ dsu.cpp
@@ -0,0 +1,17 @@
+struct DSU {
+    vector<int> par, sz;
+    void init(int n) {
+        par.resize(n);
+        sz.assign(n, 1);
+        iota(all(par), 0);
+    }
+    int get(int u)  {return u == par[u] ? u: par[u] = get(par[u]);}
+    void merge(int u, int v) {
+        u = get(u), v = get(v);
+        if (u != v) {
+            if (sz[u] < sz[v]) swap(u, v);
+            par[v] = u;
+            sz[u]+=sz[v];
+        }
+    }
+};
