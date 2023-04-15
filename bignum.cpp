ed2d45a7af0eea2b042e5528b4e9fcea06d6074e
binminh01 <88193692+binminh01@users.noreply.github.com>
2023-04-15 10:19:40 +0700

    Update bignum.cpp

--- bignum.cpp
+++ bignum.cpp
@@ -28,61 +28,74 @@ struct bignum {
     bool operator==(const bignum &a) {
         return s == a.s;
     }
+    bool operator>=(const bignum &a) {
+        return s == a.s || s > a.s;
+    }
+    bool operator<=(const bignum &a) {
+        return s == a.s || s < a.s;
+    }
     bignum operator+(const bignum &a) {
         string x = "";
-        int l1 = s.size(), l2 = a.s.size();
-        int d = l2 - l1;
-        int carry = 0;
-        for (int i = min(l1, l2) - 1; i >= 0; i--)
-        {
-            int sum = ((s[i] - '0') + (a.s[i + d] - '0') + carry);
+        int m = s.size(), n = a.s.size();
+        int d = n - m, c = 0;
+        for (int i = min(m, n) - 1; i >= 0; i--) {
+            int sum = ((s[i] - '0') + (a.s[i + d] - '0') + c);
             x.push_back(sum % 10 + '0');
-            carry = sum / 10;
+            c = sum/10;
         }
-        if (l1 < l2) {
-            for (int i = l2 - l1 - 1; i >= 0; i--)
-            {
-                int sum = ((a.s[i] - '0') + carry);
+        if (m < n) {
+            for (int i = n - m - 1; i >= 0; i--) {
+                int sum = ((a.s[i] - '0') + c);
                 x.push_back(sum % 10 + '0');
-                carry = sum / 10;
+                c = sum/10;
             }
         } else {
-            for (int i = l1 - l2 - 1; i >= 0; i--) {
-                int sum = ((s[i] - '0') + carry);
+            for (int i = m - n - 1; i >= 0; i--) {
+                int sum = ((s[i] - '0') + c);
                 x.push_back(sum % 10 + '0');
-                carry = sum / 10;
+                c = sum/10;
             }
         }
-        if (carry) x.push_back(carry + '0');
+        if (c) x.push_back(c + '0');
+        while (sz(x) > 1 && x.back() == '0') x.pop_back();
         reverse(x.begin(), x.end());
         return bignum(x);
     }
     bignum operator-(const bignum &a) {
         string x = "";
-        int l1 = s.size(), l2 = a.s.size();
-        int d = l1 - l2;
-        int carry = 0;
-        for (int i = l2 - 1; i >= 0; i--)
-        {
-            int sub = ((s[i + d] - '0') - (a.s[i] - '0') - carry);
-            if (sub < 0)
-            {
+        int m = s.size(), n = a.s.size();
+        int d = m - n, c = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            int sub = ((s[i + d] - '0') - (a.s[i] - '0') - c);
+            if (sub < 0) {
                 sub = sub + 10;
-                carry = 1;
-            }
-            else carry = 0;
+                c = 1;
+            } else c = 0;
             x.push_back(sub + '0');
         }
-        for (int i = l1 - l2 - 1; i >= 0; i--)
-        {
-            if (s[i] == '0' && carry)
-            {
+        for (int i = m - n - 1; i >= 0; i--) {
+            if (s[i] == '0' && c) {
                 x.push_back('9');
                 continue;
             }
-            int sub = ((s[i] - '0') - carry);
+            int sub = ((s[i] - '0') - c);
             if (i > 0 || sub > 0) x.push_back(sub + '0');
-            carry = 0;
+            c = 0;
+        }
+        while (sz(x) > 1 && x.back() == '0') x.pop_back();
+        reverse(x.begin(), x.end());
+        return bignum(x);
+    }
+    bignum operator*(const bignum &a) {
+        if (s == "0" || a.s == "0") return bignum(0);
+        string x = "";
+        int m = s.size() - 1, n = a.s.size() - 1, c = 0;
+        for (int i = 0; i <= m + n; i++) {
+            for (int j = max(0, i - n); j <= min(i, m); j++) {
+                c+=(s[m - j] - '0')*(a.s[n - i + j] - '0');
+            }
+            x+=c % 10 + '0';
+            c/=10;
         }
         reverse(x.begin(), x.end());
         return bignum(x);
